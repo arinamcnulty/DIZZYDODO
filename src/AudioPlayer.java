1d2ddb103dbabc9155b9c05c5b2af0c8fae5a29a
LizaKlymenko <elis.klymenko@gmail.com>
2024-06-15 15:39:30 +0300

    Added background music to the main menu

---
+++ src/AudioPlayer.java
@@ -0,0 +1,36 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+public class AudioPlayer {
+    private Clip clip;
+
+    // Correct method declaration with a proper parameter
+    public void playMusic(String filePath) {
+        try {
+            // Correct file path with double backslashes and quotes
+            File musicPath = new File("C:\\Users\\user\\Desktop\\DIZZYDODO\\src\\Angry-Birds-Theme-Song.wav");
+            if (musicPath.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY); // To loop the music
+            } else {
+                System.out.println("Can't find file");
+            }
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopMusic() {
+        if (clip != null) {
+            clip.stop();
+        }
+    }
+}
